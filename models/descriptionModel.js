da2d594b2e2a73468fc04269ab14095409f16597
Eugene <eugene_chernyshev@yahoo.com>
2024-08-12 08:57:19 +0300

    About me and job experiences (prepearing)
    The otherDescriptionList.js with about me and job experience description will be added

---
+++ models/descriptionModel.js
@@ -0,0 +1,10 @@
+const otherDescriptionList = [
+    {
+        'title': 'About me',
+        'description': 'Programming for me is a good and convenient tool that I can use to update and improve something as I work. And I like it. That\'s why I like to program in my spare time. Today, I study and practice full stack applications based on Django + Django Rest Framework + Vue.js as a powerful back-end and a simple and advanced interface to create high-level scalable projects'
+    },
+    {
+        'title': 'Job experience',
+        'description': 'I\'m improving job processes during I have this job, by my programmer skills. For example, at Oriflame, for my coworkers, I created a FAQ on all questions we had when customers called our contact center. New operators have used this instrument as helpers during customer consultations. Or another instrument that collected all information about daily events in Oriflame. This instrument had to help the operator didn`t forget important information during customer consultation because it was a very big problem for us. Before Oriflame, at "Almaz CNC", I developed the customer support system, instructions, and design for the new store page. In my last job - at DD-Cargo, I chose a Django Framework to update and improve the system and processes we had on this job. Unfortunately, not all operations were comfortable for use there. So, I learned a Django Framework for creating a system that collects all processes in one "window" instead of thousand of Google Spreadsheets and Documents. Now I continue learning Django Framework. My next goal it\'s learning VUE.JS for connecting to Django Framework using Django Rest Framework. Therefore, all my jobs have been tangential to my programming desire and desire all update and improve'
+    },
+]
