66cb0ba0909cedd71c4c6d0b973deaa082c05c63
Eugene <eugene_chernyshev@yahoo.com>
2024-08-22 07:41:25 +0300

    Contacts Modal Window (Update)
    Will be added the email and phones

--- components/contacts-window.js
+++ components/contacts-window.js
@@ -1,4 +1,17 @@
 function ContactsWindowShowing() {
+    function PhonesShowing(item) {    
+        const phoneItem = document.createElement('a');
+        phoneItem.className = 'regular-text';
+        phoneItem.innerText = item;
+
+
+        phoneItemsLead.appendChild(phoneItem);
+    }
+    
+    
+    const phoneList = ['+38 063 227 96 70', '+38 066 117 57 07',];
+    
+    
     const contactsWindowContainer = document.createElement('div');
     contactsWindowContainer.className = 'modal fade';
     contactsWindowContainer.id = 'contactsWindow';
@@ -19,9 +32,40 @@ function ContactsWindowShowing() {
 
 
     const contactsWindowsTitle = document.createElement('h5');
-    contactsWindowsTitle.className = 'modal-title';
+    contactsWindowsTitle.className = 'modal-title mx-auto';
     contactsWindowsTitle.id = 'contactsWindowLabel';
     contactsWindowsTitle.innerText = 'Let\'s stay in touch';
+    
+    
+    const contactsWindowContentBody = document.createElement('div');
+    contactsWindowContentBody.className = 'modal-body';
+    
+    
+    const contactsWindowContentBodyEmailTitle = document.createElement('h2');
+    contactsWindowContentBodyEmailTitle.className = 'display-5';
+    contactsWindowContentBodyEmailTitle.innerText = 'Email:';
+    
+    
+    const contactsWindowContentBodyEmailLink = document.createElement('a');
+    contactsWindowContentBodyEmailLink.setAttribute('href', 'mailto:eugene_chernyshev@yahoo.com');
+    contactsWindowContentBodyEmailLink.className = 'regular-text';
+    contactsWindowContentBodyEmailLink.innerText = 'eugene_chernyshev@yahoo.com';
+    
+    
+    const contactsWindowContentBodyPhoneTitle = document.createElement('h2');
+    contactsWindowContentBodyPhoneTitle.className = 'display-5';
+    contactsWindowContentBodyPhoneTitle.innerText = 'Phones:';
+    
+    
+    const phoneItemsLead = document.createElement('p');
+    phoneItemsLead.className = 'lead';
+    
+    
+    phoneList.forEach(item => {
+        PhonesShowing(item);
+    });
+    
+    
 
 
 
@@ -30,6 +74,13 @@ function ContactsWindowShowing() {
     contactsWindowDialog.appendChild(contactsWindowContent);
     contactsWindowContent.appendChild(contactsWindowHeader);
     contactsWindowHeader.appendChild(contactsWindowsTitle);
+    
+    contactsWindowContent.appendChild(contactsWindowContentBody);
+    contactsWindowContentBody.appendChild(contactsWindowContentBodyEmailTitle);
+    contactsWindowContentBodyEmailTitle.appendChild(contactsWindowContentBodyEmailLink);
+    
+    contactsWindowContentBody.appendChild(contactsWindowContentBodyPhoneTitle);
+    contactsWindowContentBodyPhoneTitle.appendChild(phoneItemsLead);
 }
 
 
