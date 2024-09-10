490b6d0b2906aed8b5bea3e149a19797c8c714a3
Eugene <eugene_chernyshev@yahoo.com>
2024-09-10 08:34:28 +0300

    Example Modal
    Preparing to add JS examples on the corresponding modal window

--- models/logos-js-projects-data.js
+++ models/logos-js-projects-data.js
@@ -10,3 +10,31 @@ const logosJSProjects = [
         'project_src': 'https://github.com/ychernyshev/editor-2.0',
     },
 ];
+
+
+const logosJSProjectsBottonList = [
+    {
+        'id': 'pills-js-1',
+        'data-target': '#pills-js-1',
+        'aria-controls': 'pills-js-1',
+    },
+    {
+        'id': 'pills-js-2',
+        'data-target': '#pills-js-2',
+        'aria-controls': 'pills-js-2',
+    },
+],
+      
+      
+const logosJSProjectsTabList = [
+    {
+        'id': 'pills-js-1',
+        'aria-labelledby': '',
+        'project_src': 'https://github.com/ychernyshev/editor-1.0',
+    },
+    {
+        'id': 'pills-js-2',
+        'aria-labelledby': '',
+        'project_src': 'https://github.com/ychernyshev/editor-1.0',
+    },
+];
