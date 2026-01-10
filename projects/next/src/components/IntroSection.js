b88cd48e903b213f94e1b40399421b910975d170
Eugene <eugene_chernyshev@yahoo.com>
2026-01-10 19:56:19 +0200

    Update IntroSection.js: Skills
    - Added skills list background color: #343A40
    - Changed skills names color using class name "text-white"
    - Changed skills names font weight using class name "font-weight-bold"
    - Changed skill names' style "style="text-align: center;"" to class name "text-center"

--- projects/next/src/components/IntroSection.js
+++ projects/next/src/components/IntroSection.js
@@ -19,11 +19,11 @@ export const IntroSection = {
                         <p class="lead regular-text mt-3" style="font-size: 1.3rem;">
                             I like Django and Vue.js. Firstly I have used only Django for my projects, but I saw that I need in much comfortable and reactive frontend. So, I am using both of these technologies to realize my ideas. You can see the list of technologies that I am exploring:
                         </p>
-                        <div class="row mt-5 mb-4 list-spacing list-font-size mx-auto">
-                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3" style="text-align: center;">PYTHON/DJANGO</div>
-                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3" style="text-align: center;">DJANGO REST FRAMEWORK</div>
-                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3" style="text-align: center;">JAVASCRIPT</div>
-                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3" style="text-align: center;">VUE.JS</div>
+                        <div class="row mt-5 mb-4 list-spacing list-font-size mx-auto text-white" style="background: #343A40">
+                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3 font-weight-bold text-center">PYTHON/DJANGO</div>
+                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3 font-weight-bold text-center">DJANGO REST FRAMEWORK</div>
+                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3 font-weight-bold text-center">JAVASCRIPT</div>
+                            <div class="col-xl-3 col-lg-3 col-sm-6 col-xs-6 bg-dark-custom p-3 font-weight-bold text-center">VUE.JS</div>
                         </div>
                     </div>
                 </div>
