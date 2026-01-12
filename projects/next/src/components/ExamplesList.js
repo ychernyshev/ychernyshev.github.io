783eea70e7b1a09927edf870172e9254f7e120ed
Eugene <eugene_chernyshev@yahoo.com>
2026-01-12 10:20:30 +0200

    Update ExamplesList.js: alignment
    A little alignment

--- projects/next/src/components/ExamplesList.js
+++ projects/next/src/components/ExamplesList.js
@@ -9,7 +9,7 @@ export const ExamplesList = {
 
         container.innerHTML = `
             <div class="d-flex flex-row justify-content-center">
-                <p style="transform: rotate(-90deg); margin-bottom: -4.6rem; font-size: 3.5rem; letter-spacing: 0.7rem;">EXAMPLES</p>
+                <p style="transform: rotate(-90deg); margin-top:3rem; margin-bottom: -1.6rem; font-size: 3.5rem; letter-spacing: 0.7rem;">EXAMPLES</p>
                 <p class="roboto-condensed-bold font-weight-bolder" style="margin-top: -12rem; margin-bottom: -15rem; margin-left: -15rem; font-size: 30rem; color: rgb(0, 87, 181);">4</p>
             </div>
         `
