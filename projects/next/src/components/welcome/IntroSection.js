394c0bf22d5c6fed1b2e3cb828c037dd2077690d
Eugene <eugene_chernyshev@yahoo.com>
2026-01-21 20:15:08 +0200

    Update IntroSection.js
    The description has the lighter style using the Bootstrap class "fw-lighter"

--- projects/next/src/components/welcome/IntroSection.js
+++ projects/next/src/components/welcome/IntroSection.js
@@ -14,9 +14,9 @@ export const IntroSection = {
                         <img src="https://raw.githubusercontent.com/ychernyshev/ychernyshev.github.io/main/assets/images/persona.jpg" width="78%" height="100%" alt="author">
                     </div>
                     <div class="col-xl-7 col-lg-8 col-sm-11 mt-4 text-justify">
-                        <h1 class="text-light fw-500">Welcome</h1>
-                        <h3 id="my-name" class="text-justify text-light fw-400">My name is <span class="text-warning fw-500">Eugene</span> or <span class="text-success fw-500">Yevhenii</span>, as you like</h3>
-                        <p class="lead regular-text mt-3" style="font-size: 1.3rem;">
+                        <h1 class="text-light">Welcome</h1>
+                        <h3 id="my-name" class="text-justify text-light fw-400">My name is <span class="text-warning">Eugene</span> or <span class="text-success">Yevhenii</span>, as you like</h3>
+                        <p class="lead regular-text mt-3 fw-lighter" style="font-size: 1.3rem;">
                             I like Django and Vue.js. Firstly I have used only Django for my projects, but I saw that I need in much comfortable and reactive frontend. So, I am using both of these technologies to realize my ideas. You can see the list of technologies that I am exploring:
                         </p>
                         <div class="row justify-content-center" id="download-cv"></div>
